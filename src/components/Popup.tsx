b560d0e637f25c2384c3f6a2d22590f68658f813
sixtyoneeightyjake <kn9ysmd9q2@privaterelay.appleid.com>
2026-01-29 23:07:38 -0600

    Dark mode Popup component

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -28,20 +28,20 @@ const Popup = forwardRef<HTMLDivElement | null, PopupProps>(function Popup(
       className={clsx(
         open ? 'block' : 'hidden',
         `h-[${height}px]`,
-        'z-10 bg-white absolute top-auto left-8 max-w-90 w-70 sm:w-90 rounded-lg shadow-[0_1px_2px_0_rgba(60,_64,_67,_.3),_0_2px_6px_2px_rgba(60,_64,_67,_.15)]',
+        'z-10 bg-nj-grey-950 border border-nj-grey-800 absolute top-auto left-8 max-w-90 w-70 sm:w-90 rounded-lg shadow-2xl',
         className
       )}
     >
       {title && (
-        <div className="flex items-center text-meet-black pt-3 pl-6 pb-0 pr-3">
+        <div className="flex items-center text-white pt-3 pl-6 pb-0 pr-3">
           <div className="text-lg leading-6 grow my-[15px] tracking-normal">
             {title}
           </div>
           <button
             onClick={closePopup}
-            className="bg-transparent hover:bg-[#0000000a] rounded-full"
+            className="bg-transparent hover:bg-nj-grey-800 rounded-full transition-colors"
           >
-            <div className="w-12 h-12 p-3">
+            <div className="w-12 h-12 p-3 text-nj-grey-400">
               <Close />
             </div>
           </button>
